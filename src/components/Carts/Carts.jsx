643fbfa2048f205c2832b7fd29b28d44ccbd66d7
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2024-06-02 14:36:05 +0600

    remaining course

--- src/components/Carts/Carts.jsx
+++ src/components/Carts/Carts.jsx
@@ -4,7 +4,7 @@ import Cart from '../Cart/Cart';
 const Carts = ({selects,credits,prices,remaining}) => {
     return (
         <div className='m-4 p-4'>
-            <h1 className='font-bold text-blue-600'>Credit Hour remaining:{remaining} </h1>
+            <h1 className='font-bold text-blue-600'>Credit Hour remaining {remaining} hr </h1>
             <h1 className='text-2xl font-bold'>Course Name</h1>
             {/* {selects.map(select=><p>{select}</p>)} */}
             {selects.map((select,idx)=><Cart select={select} key={idx}></Cart>)}
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,40 +3,47 @@ import Course from '../Course/Course';
 import Carts from '../Carts/Carts';
 
 const Courses = () => {
-    const [courses,setCourses]=useState([])
-    const [selects,setSelects]=useState([])
-    const [credits,setCredits]=useState(0)
-    const [prices,setPrices]=useState(0)
-    const [remaining,setRemaning]=useState(20)
-   useEffect(()=>{
-    fetch('course.json')
-    .then(res=>res.json())
-    .then(data=>{
-        
-        setCourses(data)
-    })
-   },[])
-   const handleSelect=(name,credit,price,id)=>{
-    setSelects([...selects,name])
-    // const showSelect=selects.filter(sel=>sel.id!==id)
-    // setSelects(showSelect);
-    const showCredit=credits+credit;
-    if(showCredit>20){
-        alert("You cant add more than 20 credits")
+    const [courses, setCourses] = useState([])
+    const [selects, setSelects] = useState([])
+    const [credits, setCredits] = useState(0)
+    const [prices, setPrices] = useState(0)
+    const [remaining, setRemaning] = useState(20)
+    useEffect(() => {
+        fetch('course.json')
+            .then(res => res.json())
+            .then(data => {
+
+                setCourses(data)
+            })
+    }, [])
+    const handleSelect = (name, credit, price, id) => {
+        setSelects([...selects, name])
+        // const showSelect=selects.filter(sel=>sel.id!==id)
+        // setSelects(showSelect);
+        const showCredit = credits + credit;
+        if (showCredit > 20) {
+            alert("You cant add more than 20 credits")
+        }
+        else {
+            setCredits(showCredit)
+        }
+
+        setPrices(prices + price)
+        const showRemaining=20-showCredit;
+        if(showRemaining<0){
+            alert("Something went wrong")
+        }
+        else{
+            setRemaning(showRemaining)
+        }
     }
-    else{
-        setCredits(showCredit)
-    }
-    
-    setPrices(prices+price)
-   }
-   
-   
+
+
 
     return (
         <div className='mt-4 flex bg-gray-200'>
             <div className='w-3/4 grid grid-cols-3 gap-4 '>
-                {courses.map(course=><Course course={course} handleSelect={handleSelect} key={course.id}></Course>)}
+                {courses.map(course => <Course course={course} handleSelect={handleSelect} key={course.id}></Course>)}
             </div>
             <div className='w-1/4'>
                 <Carts selects={selects} credits={credits} remaining={remaining} prices={prices}></Carts>
