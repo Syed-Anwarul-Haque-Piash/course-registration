815fe15c049acf8bda186d5e3a1af814d56466e6
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2024-06-02 10:37:53 +0600

    header and json data

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-center font-bold'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
